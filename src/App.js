ab30edf30928a49735d278f3bb72103ecee9acb4
Meena Haque <meena@Meenas-MacBook-Pro.local>
2022-02-22 16:41:07 +0000

    import components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 function App() {
   return (
