8c5e467f110e4bd9f45a9f8a018220e826bd48b5
Meena Haque <meena@Meenas-MacBook-Pro.local>
2022-02-22 19:11:08 +0000

    add comps from mat-ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
 const Header = () => {
-  return <h2>Header</h2>;
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          My Resturant Finder
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h8" className={classes.title}>
+            Explore
+          </Typography>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase placeholder="Search..."></InputBase>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
