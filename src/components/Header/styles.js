4c95b9029746c39e7da5ce307083c0954838766e
Meena Haque <meena@Meenas-MacBook-Pro.local>
2022-02-22 19:11:51 +0000

    add styles to header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
