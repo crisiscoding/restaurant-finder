db6cb4b4e1351adf817fa824b2d8c4c3f84b6d4a
Meena Haque <meena@Meenas-MacBook-Pro.local>
2022-02-22 17:15:18 +0000

    add grid and dummy comps with screen breakpoints

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
@@ -6,7 +7,16 @@ import Map from "./components/Map/Map";
 function App() {
   return (
     <div className="App">
-      <h1>Restaurant Finder</h1>
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        {/* Map is larger than list so needs to take less space on mobile i.e 8/12 spaces  */}
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
     </div>
   );
 }
