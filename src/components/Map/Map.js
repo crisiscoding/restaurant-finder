c384d9413dff6dfd2478e54b89c0543386543010
Meena Haque <aneemqah@gmail.com>
2022-02-24 16:03:25 +0000

    add map coordinates

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,26 +1,25 @@
 import React from "react";
-import GoogleMapReact from "google-map-react";
-import { useMediaQuery } from "@material-ui/core";
-
+import GoggleMapReact from "google-map-react";
 import useStyles from "./styles";
 
-const Map = () => {
-  const classes = useStyles(); // Use styles as function (css in js)
-  const isMobile = useMediaQuery("min-width:600px"); // If larger than 600px set isMobile variable to false
-  const coordinates = { lat: 0, long: 0 }; // Set temporary long and lat coordinates as I dont have real cords yet!
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const classes = useStyles(); // Use styles as function
 
   return (
-    // <div className={classes.mapContainer}>
-    <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBW9zun7CSCYYDwdsZzm9p3L9Pca-Ajzjg" }}
-        defaultCenter={coordinates} // center map & pass co-ordinates
-        center={coordinates} // this is the current center of the map [?]
+    <div className={classes.mapContainer}>
+      <GoggleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAzG7oTs6mqr8EuG68kzsqSVIvV67bygSM" }}
+        defaultCenter={coordinates} // default center map & pass co-ordinates
+        center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]} // top, right, bottom, left
         options={""}
-        onChange={""}
-      ></GoogleMapReact>
+        onChange={(e) => {
+          // console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+      ></GoggleMapReact>
     </div>
   );
 };
