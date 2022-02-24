88d04f928937fda2735a5297f4443bdb05481dc1
Meena Haque <meena@Meenas-MacBook-Pro.local>
2022-02-24 14:01:26 +0000

    add theme back in

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { alpha, makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   title: {
@@ -7,6 +7,21 @@ export default makeStyles((theme) => ({
       display: "block",
     },
   },
+
+  search: {
+    position: "relative",
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+
   searchIcon: {
     padding: theme.spacing(0, 2),
     height: "100%",
@@ -19,5 +34,16 @@ export default makeStyles((theme) => ({
   toolbar: {
     display: "flex",
     justifyContent: "space-between",
+  },
+
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: { width: "20ch" },
   },
 }));
