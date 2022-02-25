955e9daa226996593e64c30e0e3aabb6e3fa75e5
Meena Haque <aneemqah@gmail.com>
2022-02-25 21:54:43 +0000

    update comments

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,20 +3,20 @@ import GoggleMapReact from "google-map-react";
 import useStyles from "./styles";
 
 const Map = ({ setCoordinates, setBounds, coordinates }) => {
-  const classes = useStyles(); // Use styles as function
+  const classes = useStyles();
 
   return (
     <div className={classes.mapContainer}>
       <GoggleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-        defaultCenter={coordinates} // default center map & pass co-ordinates
+        defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
-        margin={[50, 50, 50, 50]} // top, right, bottom, left
+        margin={[50, 50, 50, 50]}
         options={""}
         onChange={(e) => {
-          // console.log(e);
-          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          //console.log("HERE I AM", e);
+          setCoordinates({ lat: e.center.latitude, lng: e.center.longitude });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
       ></GoggleMapReact>
