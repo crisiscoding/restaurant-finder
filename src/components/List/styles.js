a5e1b1fe3195838d9a35218241f09d9f346762b9
Meena Haque <aneemqah@gmail.com>
2022-02-25 22:10:29 +0000

    add styles for list comp

---
+++ src/components/List/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    marginBottom: "30px",
+  },
+  container: {
+    padding: "25px",
+  },
+  list: {
+    height: "75vh",
+    overflow: "auto",
+  },
+}));
