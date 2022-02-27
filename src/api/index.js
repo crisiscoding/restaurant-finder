593491007b4ffc36cf9e705e865f62a6b9039b7a
Meena Haque <aneemqah@gmail.com>
2022-02-27 18:12:47 +0000

    make coordinates dynamic

--- src/api/index.js
+++ src/api/index.js
@@ -3,25 +3,28 @@ import axios from "axios";
 const URL =
   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-export const getRestaurantData = async () => {
+export const getRestaurantData = async (sw, ne) => {
   try {
     const {
       data: { data },
     } = await axios.get(URL, {
       params: {
-        bl_latitude: "51.2867602",
-        tr_latitude: "51.6918741",
-        bl_longitude: "-0.5103751",
-        tr_longitude: "0.3340155",
-        // NEED TO MAKE THIS DYNAMIC - NOT WORKING ATM
-        // bl_latitude: sw.lat,
-        // tr_latitude: ne.lat,
-        // bl_longitude: sw.lng,
-        // tr_longitude: ne.lng,
+        // HARD CODE COORDS
+        // bl_latitude: "51.507351",
+        // tr_latitude: "51.6918741",
+        // bl_longitude: "-0.127758",
+        // tr_longitude: "-0.127758",
+
+        // MAKE COORDS DYNAMIC
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
       },
       headers: {
         "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
         "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY,
+        "Retry-After": 3600,
       },
     });
 
