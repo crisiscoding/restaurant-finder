a78fe59fa25b88f16c43fcb216f7eb40d393152d
Meena Haque <aneemqah@gmail.com>
2022-02-28 14:50:55 +0000

    update styles

---
+++ src/components/Details/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  subtitle: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: "10px",
+  },
+  spacing: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+}));
