ad288f69cd6d367bda5af75b97fa539bd1bfc08c
Meena Haque <aneemqah@gmail.com>
2022-03-01 12:47:13 +0000

    autocomplete search field

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import { Autocomplete } from "@react-google-maps/api";
 import SearchIcon from "@material-ui/icons/Search";
 import useStyles from "./styles";
 
-const Header = () => {
+const Header = ({ setCoordinates }) => {
+  // {setCoordinates}
   const classes = useStyles();
+
+  const [autocomplete, setAutocomplete] = useState(null);
+
+  const onLoad = (autoComp) => setAutocomplete(autoComp);
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });
+    // What do i want to do once get new place [?] -> set it to the state!
+    // Need to change the coordinates in app.js by passing setCoordinates as props to the Header
+  };
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -13,20 +26,20 @@ const Header = () => {
           My Resturant Finder
         </Typography>
         <Box display="flex">
-          {/* <Typography variant="h6" className={classes.title}>
-            Explore new restaurants
-          </Typography> */}
-          {/* <Autocomplete> NEED TO FIX THIS*/}
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+          {/* what happens when we load the auto complete component - onLoad handler  */}
+          {/* what happens when we change the place - onChange handler */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
             </div>
-            <InputBase
-              placeholder="Search..."
-              classes={{ root: classes.inputRoot, input: classes.inputInput }}
-            />
-          </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
+          {/* Need to connect GM API */}
         </Box>
       </Toolbar>
     </AppBar>
